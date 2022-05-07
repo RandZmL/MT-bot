8cd06e3233a9f14a2412e47b1cc62544b7e71d14
ZmL <alexsila01@mail.ru>
2022-05-07 04:18:34 +0300

    Обновил предыдущий код: немного оптимизировал и сделал его рабочим

--- mtbot.py
+++ mtbot.py
@@ -55,26 +55,23 @@ async def top(ctx, bw : str, diopozon = 10):
  
 async def stats(ctx, player_name : discord.Member):
 	user_id = '\\' + str(player_name.mention)
-	user_discord_name = str(player_name.mention).replace('@', '')[:-5]
+	user_discord_name = str(player_name).replace('@', '')[:-5]
 	cursor = db.cursor()
-	search_result = []
 	information = []
-	uuid : str
-
-	cursor.execute("SELECT discord_id FROM bw_stats_players")
-
-	for x in cursor:
-		search_result += x
+	uuid = []
+	emb = discord.Embed(title = '', description = '', colour = discord.Color.blue())
+	kd = float 
+	winrate : float
 
 	try:
-		cursor.execute("SELECT uuid FROM bw_stats_players WHERE discord_id = " + user_id)
+		cursor.execute("SELECT uuid FROM bw_stats_players WHERE discord_id = '" + user_id + "'")
 		for x in cursor:
 			uuid += x
 
-		cursor.execute("SELECT * FROM bw_stats_players WHERE uuid = '106b4fba-0c66-3753-b6db-8023a74bc345'")
+		cursor.execute("SELECT * FROM bw_stats_players WHERE uuid = '" + uuid[0] + "'")
 		for x in cursor:
 			information += x
-		cursor.execute("SELECT * FROM bw_rang_texted WHERE uuid = '106b4fba-0c66-3753-b6db-8023a74bc345'")
+		cursor.execute("SELECT * FROM bw_rang_texted WHERE uuid = '" + uuid[0] + "'")
 		for x in cursor:
 			information += x
 
@@ -82,22 +79,25 @@ async def stats(ctx, player_name : discord.Member):
 		information.remove(information[10])
 		information.remove(information[10])
 
-		kd = information[0]/information[6]
+		if information[6] == 0 or information[6] == 1:
+			kd = information[0]
+		else:
+			kd = information[0]/information[6]
+		
 		winrate = (information[1]/(information[1] + information[3])) * 100
 
-		emb = discord.Embed(title = 'Информация о пользователе' + user_discord_name, description = 'Профиль игрока в режиме Bed Wars', colour = discord.Color.blue())
-		emb.set_author(name = 'Ваш верный слуга - Пугало.', icon_url = bot.user.avatar_url)
+		emb = discord.Embed(title = 'Информация о пользователе ' + user_discord_name, description = 'Профиль игрока в режиме Bed Wars', colour = discord.Color.blue())
+		emb.set_author(name = information[4] + ': ' + information[9], icon_url = player_name.avatar_url)
 		emb.set_thumbnail(url = player_name.avatar_url)
-		emb.add_field(name = 'Текущий ранг: ' + information[9], value = '*Счёт в сезоне: ' + information[2] + '*')
-		emb.add_field(name = 'К/Д ' + kd, value = '*Убийств: ' + information[0] + '*\n*Смертей: ' + information[6])
-		# if information[2] > 10000:
-		# 	emb.add_field(name = 'Текущее место среди лиги лучших: ' + information[x], value = '*Разрыв: ' + information[x] + ' очков*')
-		emb.add_field(name = 'Винрейт: ' + winrate + '%', value = '*Побед: ' + information[1] + '*')
+		emb.add_field(name = 'Ранг: ' + information[9], value = '*Счёт в сезоне: ' + str(information[2]) + '*')
+		emb.add_field(name = 'К/Д: ' + str(kd), value = '*Убийств: ' + str(information[0]) + '*\n*Смертей: ' + str(information[6]) + '*')
+		# if information[2] > 10000:                                                                                                                       Алгаритм на будущее. Скорее всего, возьму за основу
+		# 	emb.add_field(name = 'Текущее место среди лиги лучших: ' + information[x], value = '*Разрыв: ' + information[x] + ' очков*')                   алгоритм из команды выше.
+		emb.add_field(name = 'Винрейт: ' + str(winrate) + '%', value = '*Побед: ' + str(information[1]) + '*')
+		await ctx.send(embed = emb)
 	except Exception as e:
-		print('Ошибка при использовании команды .stats: {e}')
+		print('Ошибка при использовании команды .stats: ' + str(e) + '\ninfo: ' + str(information) + '\nuuid: ' + str(uuid))
 		await ctx.send('**Ошибка:** пользователь не найден. Возможно, ты допустил ошибку в никнейме, или же такого игрока нет на сервере.')
-	
-	await ctx.send(embed = emb)
 
 
 @bot.command()                               #Влад (команда должна синхронизировать пользователя в discord'е с его аккаунтом в minecraft'e (по факту, нужно добавить профиль дс-а в бд
